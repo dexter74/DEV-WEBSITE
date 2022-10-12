d1b1777a5161f2dd258daffa3e378f601e8f6a3f
Marc <teste74@hotmail.fr>
2022-10-12 18:56:49 +0200

    up

---
+++ HTML/SITE/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Website par Marc Jaffré</title>
+        <link rel="stylesheet" href="./css/0_base.css"/>
+        // <link rel="stylesheet" href="./css/1_top.css"/>
+        // <link rel="stylesheet" href="./css/2_ann.css"/>
+        // <link rel="stylesheet" href="./css/3_mid.css"/>	
+        // <link rel="stylesheet" href="./css/4_bot.css"/>
+        <link rel="stylesheet" href="./css/9_responsive.css"/>
+    </head>
+	<body>
+	<div id='GLOBAL'>
+	<!-- ################################################################ -->
+		<div id='TOP'>
+			<div class='TOP 1'> <a href='#'>Accueil</a></div>
+			<div class='TOP 2'> <a href='#'>NAS</a></div>
+			<div class='TOP 3'> <a href='#'>Proxmox</a></div>
+			<div class='TOP 4'> <a href='#'>Documents</a></div>
+			<div class='TOP 5'> <a href='#'>Images</a></div>
+			<div class='TOP 6'> <a href='#'>Musique</a></div>
+			<div class='TOP 7'> <a href='#'>Téléchargements</a></div>
+			<div class='TOP 8'> <a href='#'>Vidéos</a></div>
+		</div>
+		<!-- ################################################################ -->
+		<div id='ANN'> <h2>Liens Utiles </h2>
+		<p align='left'>
+			- [Github] <a href='https://github.com/dexter74/DEV-WEBSITE'>DEV-WEBSITE</a>
+			<br />
+			
+			- [HTML]
+				 <a href='https://github.com/dexter74/DEV-WEBSITE/blob/main/HTML/Doc/Partie_I.MD'>I</a> |
+				 <a href='https://github.com/dexter74/DEV-WEBSITE/blob/main/HTML/Doc/Partie_II.MD'>II</a> |
+				 <a href='https://github.com/dexter74/DEV-WEBSITE/blob/main/HTML/Doc/Partie_III.MD'>III</a> |
+		</p>
+		</div>
+		<!-- ################################################################ -->
+		<div id='MID'>
+			<div class='MID 1'>
+			<!-- ############################################################ -->
+				<div class='ARTICLE'> 
+					<h1>Avancement du projet<h1>
+				</div>
+			<!-- ############################################################ -->
+				
+				<div class='CONTENU'> 
+				<br />
+			<img src='/image/avatar.jpg' width='50' height='50'>
+				<p align='left'>
+					---------------------------------------------------------------
+					<br />
+					[Changelog] 12/10/2022
+					<br />
+					<br />
+					- [HTML] Création de la Structure du site: 90%
+					<br />
+					- [HTML] Mise en forme du site: 100%
+					<br />
+					- [SQL] Structurer les Tables, colonnes: 0% 
+					<br />
+					- [SQL] Création de la BDD: 0% 
+					<br />
+					- [PHP] Codage en PHP: 0% 
+					<br />
+					---------------------------------------------------------------
+					<br />
+					[Changelog] XX/10/2022
+					<br />
+				</p>
+
+
+
+
+
+
+
+				
+				</div>
+			<!-- ############################################################ -->
+			</div>
+		</div>
+		<!-- ################################################################ -->
+		<div id='BOT'>
+				<div class='BOT'>
+					Tous droits réservés.
+				</div>
+		</div>
+		<!-- ################################################################ -->
+	</div>
+	<body>
+</html>
